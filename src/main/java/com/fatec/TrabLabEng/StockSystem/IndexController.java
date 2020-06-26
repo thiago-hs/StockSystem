3a265694e3489fbcac06e2d05214ee91fa585b99
Thiago Henrique <thiago.h.silva.98@gmail.com>
2020-06-25 22:45:23 -0300

    inital commit

---
+++ src/main/java/com/fatec/TrabLabEng/StockSystem/IndexController.java
@@ -0,0 +1,21 @@
+package com.fatec.TrabLabEng.StockSystem;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IndexController {
+
+	@GetMapping(produces="application/json")
+	public @ResponseBody  ArrayList<String> index(){
+	    ArrayList<String> teste = new ArrayList();
+
+		return teste;
+	}
+}
---
+++ src/test/java/com/fatec/TrabLabEng/StockSystem/StockSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.TrabLabEng.StockSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
