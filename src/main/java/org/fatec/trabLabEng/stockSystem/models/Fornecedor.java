a7aed8db5534865a1ec8183c2af4f024ca60dad3
Martinelli-Vitor <vitormartinellilobo@gmail.com>
2020-06-27 16:06:46 -0300

    relacionamento tabela

---
+++ src/main/java/org/fatec/trabLabEng/stockSystem/models/Fornecedor.java
@@ -0,0 +1,105 @@
+package org.fatec.trabLabEng.stockSystem.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Fornecedor implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int codFornecedor;
+	
+	private String nomeFornecedor;
+	private String endereco;
+	private String bairro;
+	private String cep;
+	private String email;
+	private String cnpj;
+	private String tel;
+	
+	@ManyToOne
+	private Cidade codCidade;
+
+	public int getCodFornecedor() {
+		return codFornecedor;
+	}
+
+	public void setCodFornecedor(int codFornecedor) {
+		this.codFornecedor = codFornecedor;
+	}
+
+	public String getNomeFornecedor() {
+		return nomeFornecedor;
+	}
+
+	public void setNomeFornecedor(String nomeFornecedor) {
+		this.nomeFornecedor = nomeFornecedor;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public Cidade getCodCidade() {
+		return codCidade;
+	}
+
+	public void setCodCidade(Cidade codCidade) {
+		this.codCidade = codCidade;
+	}
+	
+	
+
+}
