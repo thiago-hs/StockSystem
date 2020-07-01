11e35ca54381bc4e9e54cf62c9fbd682096aa9bb
Thiago Henrique <thiago.h.silva.98@gmail.com>
2020-06-30 21:33:05 -0300

    Reotorno para spa

--- src/main/java/org/fatec/trabLabEng/stockSystem/controllers/IndexController.java
+++ src/main/java/org/fatec/trabLabEng/stockSystem/controllers/IndexController.java
@@ -2,11 +2,15 @@ package org.fatec.trabLabEng.stockSystem.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping("/*")
+	@RequestMapping(
+	    value = { "/", "/*","/*/*" }, 
+		method = RequestMethod.GET
+	)
 	public String index(){
 		return "index";
 	}
